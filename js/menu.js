24f64a1976d9db6d5adc5443e1884fa5c3370f93
Masse Armand <armand.masse@inetum.com>
2023-12-20 10:51:31 +0100

    Redesign Accueil + Fiche Produit

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,32 @@
-const menuNavbar = document.querySelector('.menu-navbar');
-const menuBurger = document.querySelector('.menu-burger');
+const menuNavbar = document.querySelector(".menu-navbar");
+const menuBurger = document.querySelector(".menu-burger");
 
 function checkScreenSize() {
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        menuNavbar.style.display = 'none';
-        menuBurger.style.display = 'block';
-    } else {
-        menuNavbar.style.display = 'block';
-        menuBurger.style.display = 'none';
-    }
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    menuNavbar.style.display = "none";
+    menuBurger.style.display = "block";
+  } else {
+    menuNavbar.style.display = "block";
+    menuBurger.style.display = "none";
+  }
 }
 
-window.addEventListener('load', checkScreenSize);
-window.addEventListener('resize', checkScreenSize);
+function toggleMenu() {
+  var menuItems = document.querySelector(".menu-items");
+  if (menuItems.style.display === "none") {
+    menuItems.style.display = "block";
+    menuItems.style.transform = "translateX(-100%)";
+    setTimeout(() => {
+      menuItems.style.transform = "translateX(0)";
+    }, 0);
+    menuItems.style.transition = "transform 0.3s ease-in-out";
+  } else {
+    menuItems.style.transform = "translateX(-100%)";
+    setTimeout(() => {
+      menuItems.style.display = "none";
+    }, 300);
+  }
+}
+
+window.addEventListener("load", checkScreenSize);
+window.addEventListener("resize", checkScreenSize);
--- templatev2/js/menu.js
+++ templatev2/js/menu.js
@@ -1,20 +1,20 @@
 function myAccFunc() {
   var x = document.getElementById("demoAcc");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+  if (x.className.indexOf(" show") == -1) {
+    x.className += " show";
   } else {
-    x.className = x.className.replace(" w3-show", "");
+    x.className = x.className.replace(" show", "");
   }
 }
 
 document.getElementById("myBtn").click();
 
-function w3_open() {
+function open_menu() {
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("myOverlay").style.display = "block";
 }
 
-function w3_close() {
+function close_menu() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
 }
