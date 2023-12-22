367095de67c745672d5b19bcd431152e7722ba1a
Masse Armand <armand.masse@inetum.com>
2023-12-22 14:02:46 +0100

    Essai Sans Clavier

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,7 @@
+let product_focus = false;
+let children_focus = false;
+let mouse_out = true;
+
 function myAccFunc() {
   var x = document.getElementById("demoAcc");
   if (x.className.indexOf(" show") == -1) {
@@ -18,3 +22,27 @@ function close_menu() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
 }
+
+// Navigation Clavier
+/*
+for (let product of document.getElementsByClassName("product")) {
+  mouse_out = true;
+  product.addEventListener("mouseover", () => {
+    mouse_out = false;
+  });
+  if (mouse_out) {
+    product.addEventListener("focus", () => {
+      displayBuy();
+    });
+    product.children[0].children[1].addEventListener("focus", () => {
+      displayBuy();
+    });
+  }
+}
+function displayBuy() {
+  this.children[0].children[1].style.display = "block";
+}
+function hideBuy() {
+  this.children[0].children[1].style.display = "none";
+}
+*/
